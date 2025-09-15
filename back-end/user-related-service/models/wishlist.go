b76d8bbec8a3bf6bc6e1765fe540fdf6798c6ced
Sofroniebruh <sofronie.albu1@student.nhlstenden.com>
2025-09-15 13:48:34 +0200

    fix: add migrations to the user-service

---
+++ back-end/user-related-service/models/wishlist.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Wishlist struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
+	ProductID uuid.UUID `json:"product_id" db:"product_id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+}
